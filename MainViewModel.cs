7c175857d9fb9af5544327fae54b6e2b1f4ad24b
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-12-02 17:16:57 -0700

    Fix MainViewModel errors
    Now should handle if the wrong thread is calling for the UI update

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -1,42 +1,57 @@
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Microsoft.UI.Dispatching;
 
 namespace Dynamic_Lighting_Key_Indicator
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        private readonly DispatcherQueue _dispatcherQueue;
+
+        public MainViewModel()
+        {
+            // Get the dispatcher queue for the current thread (should be UI thread)
+            _dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+        }
+
         private string _deviceStatusMessage;
         public string DeviceStatusMessage
         {
-            get { return _deviceStatusMessage; }
-            set
-            {
-                if (_deviceStatusMessage != value)
-                {
-                    _deviceStatusMessage = value;
-                    OnPropertyChanged(nameof(DeviceStatusMessage));
-                }
-            }
+            get => _deviceStatusMessage;
+            set => SetProperty(ref _deviceStatusMessage, value);
         }
 
         private string _deviceWatcherStatusMessage;
         public string DeviceWatcherStatusMessage
         {
-            get { return _deviceWatcherStatusMessage; }
-            set
-            {
-                if (_deviceWatcherStatusMessage != value)
-                {
-                    _deviceWatcherStatusMessage = value;
-                    OnPropertyChanged(nameof(DeviceWatcherStatusMessage));
-                }
-            }
+            get => _deviceWatcherStatusMessage;
+            set => SetProperty(ref _deviceWatcherStatusMessage, value);
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        protected virtual void OnPropertyChanged(string propertyName)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (_dispatcherQueue.HasThreadAccess)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            else
+            {
+                _ = _dispatcherQueue.TryEnqueue(() =>
+                {
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+                });
+            }
+        }
+
+        protected bool SetProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
         }
     }
 }
