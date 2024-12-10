4c59b674775c6c1eaba2ff90ba40ed84f6fbe320
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2024-12-10 14:02:58 -0700

    Move repetitive if statements into loop

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -633,44 +633,35 @@ namespace Dynamic_Lighting_Key_Indicator
                 // If updating the DefaultColor, check the ColorSettings for which keys are set to sync to default, and update their buttons too
                 if (colorPropertyName == "DefaultColor")
                 {
-                    FontIcon fontIcon;
-                    SolidColorBrush iconColor;
-                    // This janky, should be refactored to a loop with a list of keys but this will work for now
-                    if (ViewModel.ColorSettings.SyncNumLockOnColor)
-                    { 
-                        buttonNumLockOn.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonNumLockOn);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonNumLockOn);
-                    }
-                    if (ViewModel.ColorSettings.SyncNumLockOffColor)
-                    { 
-                        buttonNumLockOff.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonNumLockOff);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonNumLockOff);
-                    }
-                    if (ViewModel.ColorSettings.SyncCapsLockOnColor)
+                    var syncPropertiesButtons = new List<(string SyncPropertyName, Button Button)>
                     {
-                        buttonCapsLockOn.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonCapsLockOn);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonCapsLockOn);
-                    }
-                    if (ViewModel.ColorSettings.SyncCapsLockOffColor)
-                    { 
-                        buttonCapsLockOff.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonCapsLockOff);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonCapsLockOff);
-                    }
-                    if (ViewModel.ColorSettings.SyncScrollLockOnColor)
-                    { 
-                        buttonScrollLockOn.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonScrollLockOn);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonScrollLockOn);
-                    }
-                    if (ViewModel.ColorSettings.SyncScrollLockOffColor)
-                    { 
-                        buttonScrollLockOff.Background = new SolidColorBrush(args.NewColor);
-                        fontIcon = GetButtonGlyphObject(buttonScrollLockOff);
-                        fontIcon.Foreground = DetermineGlyphColor(buttonScrollLockOff);
+                        ("SyncNumLockOnColor", buttonNumLockOn),
+                        ("SyncNumLockOffColor", buttonNumLockOff),
+                        ("SyncCapsLockOnColor", buttonCapsLockOn),
+                        ("SyncCapsLockOffColor", buttonCapsLockOff),
+                        ("SyncScrollLockOnColor", buttonScrollLockOn),
+                        ("SyncScrollLockOffColor", buttonScrollLockOff)
+                    };
+
+                    foreach (var (syncPropertyName, button) in syncPropertiesButtons) // Iterate through the list of tuples for each button
+                    {
+                        if (button == null)
+                            continue;
+
+                        // Use reflection to get the value of the sync property
+                        var syncPropertyInfo = typeof(ColorSettings).GetProperty(syncPropertyName);
+                        if (syncPropertyInfo == null)
+                            continue;
+
+                        bool isSynced = (bool)(syncPropertyInfo.GetValue(ViewModel.ColorSettings) ?? false);
+                        if (isSynced)
+                        {
+                            button.Background = new SolidColorBrush(args.NewColor); // Update the button color to reflect the new color
+                            // Update the color of the icon / glyph based on the brightness of the new background color
+                            FontIcon? fontIcon = GetButtonGlyphObject(button);
+                            if (fontIcon != null)
+                                fontIcon.Foreground = DetermineGlyphColor(button);
+                        }
                     }
                 }
             };
